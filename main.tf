2016bbf331807b9832e3e9bc01ecd292bfefe5d3
Ronald <ronald@technative.nl>
2023-03-05 13:35:06 +0100

    TLDA-309: Initial setup of best practice DynamoDB module.

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
-# YOU MAIN CODE HERE
+resource "aws_dynamodb_table" "this" {
+  name     = var.name
+  hash_key = var.partition_key
+
+  attribute {
+    name = var.partition_key
+    type = var.partition_key_type
+  }
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = var.kms_key_arn
+  }
+
+  stream_enabled = false
+  table_class    = "STANDARD"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
-# VARIABLES
+variable "name" {
+  description = "Prefix name for DynamoDB. Must be unique within the region."
+  type        = string
+}
+
+variable "partition_key" {
+  description = "Partition key for DynamoDB table."
+  type        = string
+}
+
+variable "partition_key_type" {
+  description = "Partition key type for DynamoDB table, can be S, N or B."
+  type        = string
+}
+
+variable "kms_key_arn" {
+  description = "KMS key to use for encrypting RDS instances."
+  type        = string
+}
